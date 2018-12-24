f276dab34e7fd6e9aad3bd6b56d2cbd2bb535577
ashaunthomas <ashaunthomas@siu.edu>
2018-12-24 00:26:03 -0600

    feat(background): Adds comment to background.

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+/**
+ * Runs while extension is turned on.
+ * This is where we check for dollar amounts and append
+ * the necessary elements.
+ */
 chrome.runtime.onInstalled.addListener(function() {
     // add an action here
 });
