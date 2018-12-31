2aec15a9f032155cba556bfa5a8e1907db0dea38
Ayush Kohli <akohli96@users.noreply.github.com>
2018-12-31 14:34:44 -0600

    boiler plate

--- background.js
+++ background.js
@@ -3,6 +3,19 @@
  * This is where we check for dollar amounts and append
  * the necessary elements.
  */
+/*
+for(var i=0;i<all.length;i++){
+    if(all[i].textContent.indexOf("$")==0){
+console.log(all[i])}}
+*/
+ 
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.action == "getHTMLSource") {
+      message.innerText = request.source;
+    }
+  });
+  
+
 chrome.runtime.onInstalled.addListener(function() {
     // add an action here
 });
