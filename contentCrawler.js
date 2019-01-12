192fe2546df9b5a04388879868495c5ab9f7a984
ashaunthomas <ashaunthomas@siu.edu>
2019-01-12 16:23:49 -0600

    fixes contentCrawler: It now returns list of 'price' elements.

--- contentCrawler.js
+++ contentCrawler.js
@@ -1,18 +1,19 @@
 alert("Content crawler activated!");
 
-const FORMAT_LONG = /$[0-9]+.[0-9][0-9]/; // $XX.XX 
-const FORMAT_SHORT = /\$[0-9]+/; // $XX [POSSIBLY TOO GENERAL]
+// USE THESE FOR BETTER PRICE-FINDING IN VERSION 1.1
+// const FORMAT_LONG = /$[0-9]+.[0-9][0-9]/; // $XX.XX  
+// const FORMAT_SHORT = /\$[0-9]+/; // $XX [POSSIBLY TOO GENERAL]
+
 /**
- * Finds all elements with prices and returns them
+ * Finds all elements with dollar signs as  first character and returns them
  * @return Collection of elements that qualily
  */
 function findAmazonPrices() {
-    let html = document.getElementsByTagName["*"];
+    let html = document.getElementsByTagName("*");
     let priceElements = [];
     for (let i = 0; i < html.length; i++) {
-        let testString = html[i].innerHTML;
-        console.log("Test string: " + testString);
-        if (testString.match(FORMAT_LONG)) {
+        let dollarSign = html[i].innerHTML.charAt(0);
+        if (dollarSign == "$") {
             priceElements.push(html[i]);
         } 
     }
